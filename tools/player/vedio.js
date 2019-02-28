9cd394eedfb32f8a068a8e756d8815fee0732a1c
a512154224 <512154224@qq.com>
2019-02-28 11:11:30 +0800

    update tools

---
+++ tools/player/vedio.js
@@ -0,0 +1,8 @@
+//www.liangshitian.com
+	function player (obj) {
+		var val = document.getElementById("J_QueryVal").value;
+		if (val == '') {
+		 alert('视频URL不能为空！'); 
+		 return false;
+		}
+	  }
---
+++ tools/player/vedio/readme.txt
@@ -0,0 +1,2 @@
+url+?src=视频地址，即可调用播放，如：
+https://www.xxx.com/html5?src=https://www.xx.com/1.mp4
